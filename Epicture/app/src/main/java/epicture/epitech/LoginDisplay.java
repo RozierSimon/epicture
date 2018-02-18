019f6c3097e2ec9600ad8d57dd4e4f99a46969a2
Florent Sebag <florent.sebag@epitech.eu>
2018-02-18 01:28:22 +0100

    42

---
+++ Epicture/app/src/main/java/epicture/epitech/LoginDisplay.java
@@ -0,0 +1,60 @@
+package epicture.epitech;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginDisplay extends Activity {
+
+    private WebView mWebView;
+    public static final Pattern accessTokenPattern = Pattern.compile("access_token=([^&]*)");
+    public static final Pattern refreshTokenPattern = Pattern.compile("refresh_token=([^&]*)");
+    public static final Pattern expiresInPattern = Pattern.compile("expires_in=(\\d+)");
+
+    private void setupWebView() {
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                boolean tokensURL = false;
+                if (url.startsWith("epicture://callback")) {
+                    tokensURL = true;
+                    Matcher m;
+
+                    m = refreshTokenPattern.matcher(url);
+                    m.find();
+                    MainActivity.refreshToken = m.group(1);
+
+                    m = accessTokenPattern.matcher(url);
+                    m.find();
+                    MainActivity.accessToken = m.group(1);
+
+                    m = expiresInPattern.matcher(url);
+                    m.find();
+                    long expiresIn = Long.valueOf(m.group(1));
+                    System.out.println("COUCOU");
+                    System.out.println(MainActivity.accessToken);
+                    finish();
+                }
+                return tokensURL;
+            }
+        });
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FrameLayout root = new FrameLayout(this);
+        mWebView = new WebView(this);
+        root.addView(mWebView);
+        setContentView(root);
+
+        setupWebView();
+
+        mWebView.loadUrl("https://api.imgur.com/oauth2/authorize?client_id=" + "0d39c4b2e7b8c0a" + "&response_type=token");
+    }
+}
--- Epicture/app/src/main/java/epicture/epitech/MainActivity.java
+++ Epicture/app/src/main/java/epicture/epitech/MainActivity.java
@@ -1,16 +1,23 @@
 package epicture.epitech;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -36,7 +43,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -46,18 +56,20 @@ import java.util.regex.Pattern;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     ImgurAPI imgur;
-    private WebView mWebView;
-    private static final Pattern accessTokenPattern = Pattern.compile("access_token=([^&]*)");
-    private static final Pattern refreshTokenPattern = Pattern.compile("refresh_token=([^&]*)");
-    private static final Pattern expiresInPattern = Pattern.compile("expires_in=(\\d+)");
-
+    public static long expiresIn;
+    public static String accessToken;
+    public static String refreshToken;
+    public boolean isMyPicture;
+    public String uploadedImage;
 
 
 
@@ -109,11 +121,15 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject item = items.getJSONObject(i);
                     MainActivity.Photo photo = new MainActivity.Photo();
 
-                    if(item.getBoolean("is_album"))
-                        photo.id = item.getString("cover");
+                    if (!isMyPicture)
+                    {
+                        if(item.getBoolean("is_album"))
+                            photo.id = item.getString("cover");
+                        else
+                            photo.id = item.getString("id");
+                    }
                     else
                         photo.id = item.getString("id");
-
                     photo.title = item.getString("title");
                     photos.add(photo); // Add photo to list
                 }
@@ -175,55 +191,119 @@ public class MainActivity extends AppCompatActivity {
         rv.setAdapter(adapter);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
 
-    private void setupWebView() {
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                // intercept the tokens
-                // http://example.com#access_token=ACCESS_TOKEN&token_type=Bearer&expires_in=3600
-                boolean tokensURL = false;
-                if (url.startsWith("epicture://callback")) {
-                    tokensURL = true;
-                    Matcher m;
+        InputStream stream = null;
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            try {
+                stream = getContentResolver().openInputStream(data.getData());
+                Bitmap bitmap = BitmapFactory.decodeStream(stream);
+                // Creates Byte Array from picture
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                byte[] byteArray = baos.toByteArray();
+                String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                String baseUrl = "https://api.imgur.com/3/";
+                RequestBody requestBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("image", encoded)
+                        .addFormDataPart("type", "base64")
+                        .build();
 
-                    m = refreshTokenPattern.matcher(url);
-                    m.find();
-                    String refreshToken = m.group(1);
+                System.out.println(accessToken);
+                Request request = new Request.Builder()
+                        .url(baseUrl + "image")
+                        .addHeader("Authorization","Client-ID " + "0d39c4b2e7b8c0a")
+                        //.addHeader("Authorization", "Bearer " + accessToken)
+                        .addHeader("User-Agent","epicture")
+                        .addHeader("Accept", "application/json; q=0.5")
+                        .post(requestBody)
+                        .build();
+                try {
+                    OkHttpClient httpClient = new OkHttpClient.Builder().build();
+                    httpClient.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            Log.e("MEssage", "An unexpected error has occurred : " + e);
+                        }
 
-                    m = accessTokenPattern.matcher(url);
-                    m.find();
-                    String accessToken = m.group(1);
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            uploadedImage = null;
+                            if (response.code() == 200) {
+                                try {
+                                    String responseString = response.body().string();
+                                    JSONObject Jobject = new JSONObject(responseString);
+                                    JSONObject jobject = Jobject.getJSONObject("data");
+                                    uploadedImage = jobject.getString("link");
+                                    System.out.println("coucou");
+                                    System.out.println(uploadedImage);
+                                    Log.d("TAG", Jobject.getString("data"));
+                                    MainActivity.this.runOnUiThread(new Runnable() {
+                                        public void run() {
+                                            success_dialog(uploadedImage);
+                                        }
+                                    });
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                } finally {
 
-                    m = expiresInPattern.matcher(url);
-                    m.find();
-                    long expiresIn = Long.valueOf(m.group(1));
-                    System.out.println(accessToken);
-                    System.out.println("COUCOU");
+                                }
+                            }
+                        }
+                    });
 
                 }
-
-                return tokensURL;
+                finally {
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
-        });
+        }
+    }
+
+    private void success_dialog(String str)
+    {
+        AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Success")
+                .setMessage("Your picture has been successfully uploaded anonymously to url " + str)
+                .setView(null)
+                .setNegativeButton("OK", null)
+                .create();
+        dialog.show();
+    }
+
+    private void create_dialog_upload()
+    {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Upload an image to Imgur")
+                .setMessage("Describe the title and description before browsing your image")
+                .setView(null)
+                .setPositiveButton("BROWSE",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent();
+                                intent.setAction(Intent.ACTION_GET_CONTENT);
+                                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                                intent.setType("image/*");
+
+                                startActivityForResult(intent, 1);
+                            }
+                        })
+                .setNegativeButton("EXIT", null)
+                .create();
+        dialog.show();
     }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-
-        //imgur = new ImgurAPI();
-        FrameLayout root = new FrameLayout(this);
-        mWebView = new WebView(this);
-        root.addView(mWebView);
-        setContentView(root);
-
-        setupWebView();
-
-        mWebView.loadUrl("https://api.imgur.com/oauth2/authorize?client_id=" + "0d39c4b2e7b8c0a" + "&response_type=token");
         setContentView(R.layout.activity_main);
+
+        imgur = new ImgurAPI();
         }
 
     @Override
@@ -236,6 +316,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 searchViewAndroidActionBar.clearFocus();
+                isMyPicture = false;
                 imgur.init_request(query);
                 imgur.research();
                 refreshList();
@@ -250,5 +331,39 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_connect:
+                Intent intent = new Intent(MainActivity.this, LoginDisplay.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_upload:
+                create_dialog_upload();
+                //if (uploadedImage != null)
+                    //success_dialog(uploadedImage);
+                return true;
+            case R.id.action_myimg:
+                imgur.request = new Request.Builder()
+                        .url("https://api.imgur.com/3/account/me/images")
+                        .header("Authorization","Client-ID 0d39c4b2e7b8c0a")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .header("User-Agent","epicture")
+                        .build();
+                isMyPicture = true;
+                imgur.research();
+                refreshList();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean loggedIn = !TextUtils.isEmpty(accessToken);
+        menu.findItem(R.id.action_connect).setVisible(!loggedIn);
+        menu.findItem(R.id.action_upload).setVisible(loggedIn);
+        menu.findItem(R.id.action_myimg).setVisible(loggedIn);
+        return super.onPrepareOptionsMenu(menu);
+    }
 }
